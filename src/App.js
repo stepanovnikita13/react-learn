aaae32ba521462697bfec7a62a544598f0a309ee
stepanovnikita13 <stepanov.nikita13@gmail.com>
2022-04-21 13:36:52 +0300

    Lesson 18: Create Dialogs page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 function App() {
 	return (
 		<div className="app__grid">
 			<Header />
 			<Navbar />
-			<Profile />
+			<div className='content'>
+				{/* <Profile /> */}
+				<Dialogs />
+			</div>
 		</div>
 	);
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
 	return (
-		<div className='content'>
+		<div>
 			<div className={s.profileBg}>
 				<img src='https://digitaldefynd.com/wp-content/uploads/2020/07/Best-Abstract-Art-course-tutorial-class-certification-training-online-scaled.jpg' alt='background' />
 			</div>
