43f8da6af5e14d023bd481007bc66fd2ec8c50e2
stepanovnikita13 <stepanov.nikita13@gmail.com>
2022-04-22 13:53:56 +0300

    Lesson 24: Create primitive database

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 import s from './Dialogs.module.css'
 
 const DialogItem = (props) => (
@@ -11,21 +11,35 @@ const Message = (props) => (
 	<div className={s.item}>{props.msg}</div>
 )
 
-const Dialogs = () => (
-	<div className={s.dialogs}>
-		<div className={s.list}>
-			<DialogItem name='Nikita' id='1' />
-			<DialogItem name='Stas' id='2' />
-			<DialogItem name='German' id='3' />
-			<DialogItem name='Turbo' id='4' />
-			<DialogItem name='Djusha Metelkin' id='5' />
+const Dialogs = (props) => {
+
+	let dialogsData = [
+		{ id: 1, name: 'Nikita' },
+		{ id: 2, name: 'Stas' },
+		{ id: 3, name: 'German' },
+		{ id: 4, name: 'Turbo' },
+		{ id: 5, name: 'Djusha Metelkin' },
+	]
+
+	let msgData = [
+		{ id: 1, msg: 'Hi!' },
+		{ id: 1, msg: 'How are you?' },
+		{ id: 1, msg: 'Go to the bar!' },
+	]
+
+	return (
+		<div className={s.dialogs}>
+			<div className={s.list}>
+				<DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+				<DialogItem name={dialogsData[1].name} id={dialogsData[0].id} />
+			</div>
+			<div className={s.messages}>
+				<Message msg={msgData[0].msg} />
+				<Message msg={msgData[1].msg} />
+				<Message msg={msgData[2].msg} />
+			</div>
 		</div>
-		<div className={s.messages}>
-			<Message msg='Hi!' />
-			<Message msg='How are you?' />
-			<Message msg='Go to the bar!' />
-		</div>
-	</div>
-)
+	)
+}
 
 export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,19 +1,28 @@
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
 
-const MyPosts = () => (
-	<div className={s.postsBlock}>
-		<h3>My posts</h3>
-		<div>
-			<textarea></textarea>
-			<button>Add post</button>
-		</div>
-		<div className={s.postList}>
-			<Post msg="Hi! How are you?" />
-			<Post msg="It's my first post" />
-			<Post msg="Bye!" />
-		</div>
-	</div>
-)
+const MyPosts = () => {
+
+	let postsData = [
+		{ id: 0, text: 'Hi! How are you?', likesCount: 12 },
+		{ id: 0, text: "It's my first post", likesCount: 11 },
+		{ id: 0, text: 'Bye!', likesCount: 8 },
+	]
+
+	return (
+		<div className={s.postsBlock} >
+			<h3>My posts</h3>
+			<div>
+				<textarea></textarea>
+				<button>Add post</button>
+			</div>
+			<div className={s.postList}>
+				<Post msg={postsData[0].text} likesCount={postsData[0].likesCount} />
+				<Post msg={postsData[1].text} likesCount={postsData[1].likesCount} />
+				<Post msg={postsData[2].text} likesCount={postsData[2].likesCount} />
+			</div>
+		</ div>
+	)
+}
 
 export default MyPosts;
