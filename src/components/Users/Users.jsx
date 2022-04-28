b39dbc365215d25502cba15aa6a1032ed0910ba5
stepanovnikita13 <stepanov.nikita13@gmail.com>
2022-04-29 00:14:37 +0300

    Lesson 53: Class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,23 +1,25 @@
+import React from "react";
 import * as axios from "axios";
 import User from "./User/User";
 import s from './Users.module.css'
 
-const Users = (props) => {
-	if (props.users.length === 0) {
-		axios.get('https://social-network.samuraijs.com/api/1.0/users').then(res => {
-			console.log(res.data.items);
-			props.setUsers(res.data.items)
-		})
+class Users extends React.Component {
+	constructor(props) {
+		super(props)
+		axios.get('https://social-network.samuraijs.com/api/1.0/users')
+			.then(res => this.props.setUsers(res.data.items))
 	}
 
-	let usersList = props.users.map(u => <User key={u.id} user={u} toggleFollow={props.toggleFollow} />)
-	return (
-		<div className={s.users}>
-			<div className={s.list}>
-				{usersList}
+	render() {
+		let usersList = this.props.users.map(u => <User key={u.id} user={u} toggleFollow={this.props.toggleFollow} />)
+		return (
+			<div className={s.users}>
+				<div className={s.list}>
+					{usersList}
+				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default Users;
